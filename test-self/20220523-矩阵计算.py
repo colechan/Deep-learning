3127c33b6af0b9e6ca5be5e55ddc49e769937f4c
colechan <colechan0603@gmail.com>
2022-05-23 15:33:29 +0800

    学习临时文档

---
+++ test-self/20220523-矩阵计算.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+# Author: Cole Chan
+# Contact: chensqi@nuist.edu.cn
+# Software: Vscode
+# File: 20220523-矩阵计算.py
+# Time: 2022/05/23 15:03:29
+# Desc:
+'''
+
+# here put the import lib
+
+# %%
+form mxnet import sym
+
+a = sym.var()
+b = sym.var()
+c = 2 * a + b
